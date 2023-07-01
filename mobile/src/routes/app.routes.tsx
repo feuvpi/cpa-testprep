ea8fa5a6d1a92cba0dbfddfc8a88c21b9bacd5fa
feuvpi <fredvpg@gmail.com>
2023-07-01 19:53:18 -0300

    adding question route

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from '../screens/Login';
 import { Register } from '../screens/Register';
 import { Home } from '../screens/Home';
+import { QuestionScreen } from '../screens/QuestionScreen';
 import { Form } from '../screens/Form';
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -9,7 +10,7 @@ const { Navigator, Screen } = createNativeStackNavigator();
 export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={Home} />
+      <Screen name="Home" component={Home} />
       <Screen name="login" component={Login} />
       
       <Screen name="Form" component={Form} />
