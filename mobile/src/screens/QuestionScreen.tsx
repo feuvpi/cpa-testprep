23a28ef373fd8298e28e94df7331260f1f69d07e
feuvpi <fredvpg@gmail.com>
2023-07-06 21:27:56 -0300

    bottom buttons icons

--- mobile/src/screens/QuestionScreen.tsx
+++ mobile/src/screens/QuestionScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { Question } from '../components/Question';
+import Icon from 'react-native-vector-icons/Feather';
 
 const questionMock = {
   question: "Qual é a capital do Brasil?",
@@ -12,34 +13,36 @@ export function QuestionScreen() {
     <View className="flex-1 bg-background pt-16">
       <Question question={questionMock.question} alternatives={questionMock.alternatives} />
 
-      <View className="flex-row justify-between px-8 mt-8">
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#61C0FF',
-            borderRadius: 8,
-            padding: 12,
-            alignItems: 'center',
-            flex: 1,
-            marginRight: 4,
-          }}
-          onPress={() => handlePreviousQuestion()}
-        >
-          <Text style={{ color: '#fff', fontWeight: 'bold' }}>Questão Anterior</Text>
-        </TouchableOpacity>
+      <View className="flex-row justify-between px-8 mt-24">
+      <TouchableOpacity
+  style={{
+    backgroundColor: '#61C0FF',
+    borderRadius: 8,
+    padding: 18,
+    alignItems: 'center',
+    flex: 1,
+    marginRight: 80,
+    width: 10,
+  }}
+  onPress={() => handlePreviousQuestion()}
+>
+  <Icon name="arrow-left" size={20} color="#fff" />
+</TouchableOpacity>
 
-        <TouchableOpacity
-          style={{
-            backgroundColor: '#61C0FF',
-            borderRadius: 8,
-            padding: 12,
-            alignItems: 'center',
-            flex: 1,
-            marginLeft: 4,
-          }}
-          onPress={() => handleNextQuestion()}
-        >
-          <Text style={{ color: '#fff', fontWeight: 'bold' }}>Próxima Questão</Text>
-        </TouchableOpacity>
+<TouchableOpacity
+  style={{
+    backgroundColor: '#61C0FF',
+    borderRadius: 8,
+    padding: 18,
+    alignItems: 'center',
+    flex: 1,
+    marginLeft: 80,
+    width: 10,
+  }}
+  onPress={() => handleNextQuestion()}
+>
+  <Icon name="arrow-right" size={20} color="#fff" />
+</TouchableOpacity>
       </View>
     </View>
   );
