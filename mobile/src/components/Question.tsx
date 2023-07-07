44bf9a12047e02a2bfd61340093f62e8b9a37727
feuvpi <fredvpg@gmail.com>
2023-07-06 21:27:34 -0300

    adjusting margins

--- mobile/src/components/Question.tsx
+++ mobile/src/components/Question.tsx
@@ -8,8 +8,8 @@ type QuestionProps = {
 
 export function Question({ question, alternatives }: QuestionProps) {
   return (
-    <View className="mb-4 pt-8 mt-10">
-      <Text style={{ fontWeight: 'bold', fontSize: 16 }} className="text-zinc-400 mb-8">{question}</Text>
+    <View className="mb-4 pt-8 mt-4">
+      <Text style={{ fontWeight: 'bold', fontSize: 16 }} className="text-zinc-400 mb-24">{question}</Text>
       {alternatives.map((alternative, index) => (
         <TouchableOpacity
           key={index}
